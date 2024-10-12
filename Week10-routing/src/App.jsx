a49c5037460a01a806bdec10d17c979ebc4b387c
itanaysharma <thisistanaysharma@gmail.com>
2024-10-12 20:38:54 +0530

    Using Link which is a better way

--- Week10-routing/src/App.jsx
+++ Week10-routing/src/App.jsx
@@ -1,15 +1,22 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div>
-      {/* bad way of routing as it will reload everytime */}
-      <a href="/">ALLEN</a>| CLASS 11 | CLASS 12
+      {/* Best way to route */}
+
       <BrowserRouter>
+        <Link to="/">Allen</Link>|
+        <Link to="/neet/online-coaching-class-11">Class 11</Link>|
+        <Link to="/neet/online-coaching-class-12">Class 12</Link>
         <Routes>
           <Route
-            path="/need/online-coaching-class-11"
+            path="/neet/online-coaching-class-11"
             element={<Class11Program />}
+          />
+          <Route
+            path="/neet/online-coaching-class-12"
+            element={<Class12Program />}
           />
           <Route path="/" element={<Landing />} />
         </Routes>
@@ -22,6 +29,9 @@ function Landing() {
 }
 function Class11Program() {
   return <div>Neet program class 11th</div>;
+}
+function Class12Program() {
+  return <div>Neet program class 12th</div>;
 }
 
 export default App;
