26cd982efd17e3fda5973d9e468683caaab422b8
itanaysharma <thisistanaysharma@gmail.com>
2024-10-13 13:55:53 +0530

    Layouts and use of Outlet

--- Week10-routing/src/App.jsx
+++ Week10-routing/src/App.jsx
@@ -1,28 +1,46 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 function App() {
   return (
     <div>
       {/* Best way to route */}
+      {/* Learn useNevigate as well */}
 
       <BrowserRouter>
-        <Link to="/">Allen</Link>|
-        <Link to="/neet/online-coaching-class-11">Class 11</Link>|
-        <Link to="/neet/online-coaching-class-12">Class 12</Link>
         <Routes>
-          <Route
-            path="/neet/online-coaching-class-11"
-            element={<Class11Program />}
-          />
-          <Route
-            path="/neet/online-coaching-class-12"
-            element={<Class12Program />}
-          />
-          <Route path="/" element={<Landing />} />
+          {/* Using layout the: The nested route parent path secures that the path should always start with the string defined. */}
+          <Route path="/" element={<Layout />}>
+            <Route
+              path="/neet/online-coaching-class-11"
+              element={<Class11Program />}
+            />
+            <Route
+              path="/neet/online-coaching-class-12"
+              element={<Class12Program />}
+            />
+            <Route path="/" element={<Landing />} />
+            {/* If a route is chosen that is not defined */}
+            <Route path="*" element={<NoPage />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
   );
+}
+
+function Layout() {
+  return (
+    <div>
+      <Link to="/">Allen</Link>|
+      <Link to="/neet/online-coaching-class-11">Class 11</Link>|
+      <Link to="/neet/online-coaching-class-12">Class 12</Link>
+      {/* Outlet puts all the childs of nested route parents here */}
+      <Outlet />
+    </div>
+  );
+}
+function NoPage() {
+  return <div>Not Found</div>;
 }
 function Landing() {
   return <div>Landing page</div>;
