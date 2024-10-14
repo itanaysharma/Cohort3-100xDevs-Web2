77660d3e1be4d62589ebdd82c45f4506da8ce9a8
itanaysharma <thisistanaysharma@gmail.com>
2024-10-14 19:06:41 +0530

    Now using useRef to solve the problem

--- Week10-routing/src/Useref.jsx
+++ Week10-routing/src/Useref.jsx
@@ -1,5 +1,7 @@
 // ### What is `useRef`?
 
+import { useRef } from "react";
+
 // In React, `useRef` is a hook that provides a way to create a **reference** to a value or a DOM element that persists across renders but **does not trigger a re-render** when the value changes.
 
 // ### Key Characteristics of `useRef`:
@@ -8,16 +10,17 @@
 // 2. **No Re-Renders on Change**: Changing the value of a `ref` (`ref.current`) does **not** cause a component to re-render. This is different from state (`useState`), which triggers a re-render when updated.
 
 function UseRefUse1() {
+  const inputRef = useRef();
   function focusInput() {
     //Not recommended because
     //1. May cause inconsistency between virtual DOM and Actual DOM
     //2. Unique IDs: In React, IDs should be unique within the entire application. If you use the same id="name" somewhere else in another component, this can cause unexpected behavior.
-    document.getElementById("name").focus();
+    inputRef.current.focus();
   }
   return (
     <div>
       Sign Up
-      <input id="name" />
+      <input ref={inputRef} />
       <input />
       <button onClick={focusInput}>Focus on the input</button>
     </div>
