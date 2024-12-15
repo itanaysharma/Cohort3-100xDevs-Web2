4cfedd706240eb0346e7434fa4ef9617e514f87c
itanaysharma <thisistanaysharma@gmail.com>
2024-12-15 20:22:17 +0530

    Added nextjs backend logic

--- week19-next-app/app/page.tsx
+++ week19-next-app/app/page.tsx
@@ -1,17 +1,28 @@
 import Image from "next/image";
+import axios from "axios";
 import Link from "next/link";
+async function getUserDetails() {
+  try {
+    const response = await axios.get("http://localhost:3001/api/user");
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+export default async function Home() {
+  const userData = await getUserDetails();
 
-export default function Home() {
   return (
     <div className="text-lg w-screen h-screen flex items-center justify-center">
       <div>
         TODO APPLICATION
+        <div>{userData?.email}</div>
         <br></br>
         <Link className="text-md border m-2" href="/signin">
           Sign in to TODO app
         </Link>
         <br></br>
-        <Link className="text-md border m-2" href="/signin">
+        <Link className="text-md border m-2" href="/signup">
           Sign up to TODO app
         </Link>
       </div>
