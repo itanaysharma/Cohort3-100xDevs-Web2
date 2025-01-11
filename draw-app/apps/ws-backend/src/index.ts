999f199190ce1f8b9cfc29b406823646e7d5c523
Turbobot <turbobot@vercel.com>
2025-01-12 01:05:09 +0530

    feat(create-turbo): apply official-starter transform

---
+++ draw-app/apps/ws-backend/src/index.ts
@@ -0,0 +1,9 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(`Received message => ${message}`);
+  });
+  ws.send("Hello! Message from server.");
+});
