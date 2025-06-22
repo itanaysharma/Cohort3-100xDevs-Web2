b823db4412580f60870f647fe7d165b8f7a0817c
Tanay Sharma <thisistanaysharma@gmail.com>
2025-06-22 19:07:09 +0530

    Added the disconnect logic so it can remove that socket connection from the allwebsocket list

--- Week16-2-websockets/src/index.ts
+++ Week16-2-websockets/src/index.ts
@@ -1,17 +1,20 @@
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
-let userCount = 0;
+//@ts-ignore
 let allSockets = [];
 wss.on("connection", function (socket) {
   allSockets.push(socket);
-  userCount += 1;
-  console.log("User connected # " + userCount);
+  console.log("User connected # ");
 
   socket.on("message", (message) => {
     console.log("message recieved " + message.toString());
     for (const s of allSockets) {
       s.send(message.toString() + ": sent from the server");
     }
+  });
+
+  socket.on("disconnect", () => {
+    allSockets = allSockets.filter((x) => x != socket);
   });
 });
