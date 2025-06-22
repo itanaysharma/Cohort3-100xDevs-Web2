e2962deb249906b80414ee8ac90a33996a5baf2d
Tanay Sharma <thisistanaysharma@gmail.com>
2025-06-22 20:26:41 +0530

    Now all the messages gets delivered to a single room

--- Week16-2-websockets/dist/index.js
+++ Week16-2-websockets/dist/index.js
@@ -2,18 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
-//@ts-ignore
 let allSockets = [];
 wss.on("connection", function (socket) {
-    allSockets.push(socket);
     console.log("User connected # ");
     socket.on("message", (message) => {
-        console.log("message recieved " + message.toString());
-        for (const s of allSockets) {
-            s.send(message.toString() + ": sent from the server");
+        //this time the message will be of object looking like a string
+        const parsedMessage = JSON.parse(message.toString());
+        if (parsedMessage.type === "join") {
+            allSockets.push({ socket: socket, room: parsedMessage.roomId });
+        }
+        else if (parsedMessage.type === "chat") {
+            let room = "";
+            for (const s of allSockets) {
+                if (s.socket === socket) {
+                    room = s.room;
+                }
+            }
+            for (const ss of allSockets) {
+                if (ss.room === room) {
+                    ss.socket.send(parsedMessage.message);
+                }
+            }
         }
     });
-    socket.on("disconnect", () => {
-        allSockets = allSockets.filter((x) => x != socket);
-    });
+    //   socket.on("disconnect", () => {
+    //     allSockets = allSockets.filter((x) => x != socket);
+    //   });
 });
--- Week16-2-websockets/src/index.ts
+++ Week16-2-websockets/src/index.ts
@@ -1,20 +1,35 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
-//@ts-ignore
-let allSockets: any[] = [];
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+let allSockets: User[] = [];
 wss.on("connection", function (socket) {
-  allSockets.push(socket);
   console.log("User connected # ");
 
   socket.on("message", (message) => {
-    console.log("message recieved " + message.toString());
-    for (const s of allSockets) {
-      s.send(message.toString() + ": sent from the server");
+    //this time the message will be of object looking like a string
+    const parsedMessage = JSON.parse(message.toString());
+    if (parsedMessage.type === "join") {
+      allSockets.push({ socket: socket, room: parsedMessage.roomId });
+    } else if (parsedMessage.type === "chat") {
+      let room = "";
+      for (const s of allSockets) {
+        if (s.socket === socket) {
+          room = s.room;
+        }
+      }
+      for (const ss of allSockets) {
+        if (ss.room === room) {
+          ss.socket.send(parsedMessage.message);
+        }
+      }
     }
   });
 
-  socket.on("disconnect", () => {
-    allSockets = allSockets.filter((x) => x != socket);
-  });
+  //   socket.on("disconnect", () => {
+  //     allSockets = allSockets.filter((x) => x != socket);
+  //   });
 });
