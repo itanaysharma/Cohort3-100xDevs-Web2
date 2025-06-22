13a6e98a7993bf7642fb41f48c98bee4b30bcf19
Tanay Sharma <thisistanaysharma@gmail.com>
2025-06-22 16:17:35 +0530

    Added new websocket file

---
+++ Week16-1-websockets/src/index.ts
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+//We can connect via postnam on 8080 port and check run this
+wss.on("connection", function (socket) {
+  console.log("User Connected");
+  socket.send("User Connected");
+  socket.on("message", function (e) {
+    console.log(e.toString());
+    socket.send(e.toString());
+  });
+});
