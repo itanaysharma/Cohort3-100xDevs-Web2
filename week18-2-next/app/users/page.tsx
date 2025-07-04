cd8601740b7b6736b0c1b422281da1cd9c037352
Tanay Sharma <thisistanaysharma@gmail.com>
2025-07-05 01:10:08 +0530

    Edited the user page to get infor from online server

--- week18-2-next/app/users/page.tsx
+++ week18-2-next/app/users/page.tsx
@@ -1,5 +1,16 @@
-import Image from "next/image";
-
-export default function Home() {
-  return <div>Users page</div>;
+import axios from "axios";
+export default async function User() {
+  const response = await axios.get<{ name: string; email: string }>(
+    "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
+  );
+  const data = response.data;
+  console.log("hi");
+  return (
+    <div>
+      User page <br />
+      {data.name}
+      <br />
+      {data.email}
+    </div>
+  );
 }
