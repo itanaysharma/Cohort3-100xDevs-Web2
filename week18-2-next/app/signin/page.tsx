bc8b23656b501c90d8390bcf9a7aaf5b47dafba0
Tanay Sharma <thisistanaysharma@gmail.com>
2025-07-05 11:28:09 +0530

    Some random signin

--- week18-2-next/app/signin/page.tsx
+++ week18-2-next/app/signin/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import axios from "axios";
+import { useRef } from "react";
 export default function Signin() {
+  const userRef = useRef(null);
+  const passRef = useRef(null);
   return (
     <div className="w-screen h-screen flex text-2xl items-center justify-center">
-      <input type="text" placeholder="username"></input>
-      <input type="password" placeholder="password"></input>
+      <input ref={userRef} type="text" placeholder="username"></input>
+      <input ref={passRef} type="password" placeholder="password"></input>
       <button
         onClick={() => {
           axios.post("http://localhost:3000/api/v1/signin");
