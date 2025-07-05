ec5c957c48b78c5ddbb455872cb75b82264c7185
Tanay Sharma <thisistanaysharma@gmail.com>
2025-07-05 11:17:28 +0530

    added prisma to transfer data to prisma

--- week18-2-next/app/api/v1/signup/route.ts
+++ week18-2-next/app/api/v1/signup/route.ts
@@ -1,11 +1,25 @@
+import { PrismaClient } from "@/app/generated/prisma";
 import { NextRequest, NextResponse } from "next/server";
+
+const client = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
-
+  const { username, password } = data;
+  const user = await client.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
   console.log(data);
 
   return NextResponse.json({
     message: "You have been signed up",
   });
+}
+
+export async function GET() {
+  const user = await client.user.findFirst({});
+  return Response.json({ name: user?.username, email: user?.username });
 }
