0bc6262c74c06715608bd87c2431326a7fe0df62
Tanay Sharma <thisistanaysharma@gmail.com>
2025-07-05 10:50:58 +0530

    Added route so that when click signup it takes us to /

--- week18-2-next/app/signup/page.tsx
+++ week18-2-next/app/signup/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useRef } from "react";
 export default function Signup() {
   const userRef = useRef<HTMLInputElement>(null);
   const passRef = useRef<HTMLInputElement>(null);
-  const handleSubmit = () => {
-    axios.post("http://localhost:3000/api/v1/signup", {
+  const router = useRouter();
+  const handleSubmit = async () => {
+    await axios.post("http://localhost:3000/api/v1/signup", {
       username: userRef.current?.value,
       password: passRef.current?.value,
     });
+    router.push("/");
   };
   return (
     <div className="w-screen h-screen flex text-2xl items-center justify-center">
