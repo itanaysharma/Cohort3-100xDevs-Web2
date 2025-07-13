919e12b847dc045710a3cbbd6c16f1c62c966232
Tanay Sharma <thisistanaysharma@gmail.com>
2025-07-13 13:33:13 +0530

    Added APi endpoints and jwt 13-14 notion

--- draw-app-new/apps/http-backend/src/index.ts
+++ draw-app-new/apps/http-backend/src/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
-
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.post("/signup", (req, res) => {
+  // db call
+  res.json({
+    userId: 3,
+  });
 });
 
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+  res.json({ token });
+});
+
+app.post("/room", middleware, (req, res) => {
+  //   db call
+  res.json({
+    roomId: 12,
+  });
+});
 app.listen(3001);
