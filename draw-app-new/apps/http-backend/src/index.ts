24f5b0ada71a3df5c64c6ab824391a2bf4ec9901
Tanay Sharma <thisistanaysharma@gmail.com>
2025-08-02 18:08:41 +0530

    Added another endpooints that prints all the chats associted with a room Id

--- draw-app-new/apps/http-backend/src/index.ts
+++ draw-app-new/apps/http-backend/src/index.ts
@@ -91,4 +91,17 @@ app.post("/room", middleware, async (req, res) => {
     });
   }
 });
+app.get("/chats/:roomId", async (req, res) => {
+  const roomId = Number(req.params.roomId);
+  const messages = await prismaClient.chat.findMany({
+    where: {
+      roomId: roomId,
+    },
+    orderBy: {
+      id: "desc",
+    },
+    take: 50,
+  });
+  res.json({ messages });
+});
 app.listen(3001);
